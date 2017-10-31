79e3fec83149b124670bfffdb5d6c1b8a070421e
MakakenGH <nhitzel2@gmail.com>
2017-10-31 15:44:55 +0100

    ordnerstruktur hinzugefügt

--- functions/users/login_do.php
+++ functions/users/login_do.php
@@ -1,7 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Niklas
- * Date: 30.10.2017
- * Time: 12:20
- */
+session_start();
+include_once("../db.php");
+$db = new PDO($dsn, $dbuser, $dbpass);
+
+if(isset($_GET['login'])) {
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+
+    $statement = $db->prepare("SELECT * FROM cms_users WHERE login = :login");
+    $result = $statement->execute(array('login' => $login));
+    $user = $statement->fetch();
+
+    if ($user !== false && password_verify($password, $user['password'])) {
+        $_SESSION['userid'] = $user['id'];
+        header('Location: ../../index.php');
+    } else {
+        $errorMessage = "Login oder Passwort war ungültig<br>";
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<?php
+if(isset($errorMessage)) {
+    echo $errorMessage;
+}
+?>
+
+</body>
+</html>
