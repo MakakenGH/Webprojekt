0b5a38459235bb86346c3fa70fe930cb4d337320
MakakenGH <nhitzel2@gmail.com>
2017-11-14 17:54:54 +0100

    genre hinzugefügt

--- functions/products/create_do.php
+++ functions/products/create_do.php
@@ -6,6 +6,7 @@ $db = new PDO($dsn, $dbuser, $dbpass);
 $artikelname = $_POST["name"];
 $preis = $_POST["preis"];
 $artikelbeschreibung = $_POST["beschreibung"];
+$genre = $_POST["genre"];
 $bewertung = $_POST["rating"];
 $produktbild = $_FILES['bild']['name']; //Name der Bilddatei mit Endung
 $filename = pathinfo($_FILES['bild']['name'], PATHINFO_FILENAME);
@@ -15,10 +16,10 @@ $extension = strtolower(pathinfo($_FILES['bild']['name'], PATHINFO_EXTENSION));
 $allowed_extensions = array('png', 'jpg', 'jpeg', 'gif');
 
 //Eingaben werden in die Datenbank geladen
-if (!empty($artikelname) && !empty($preis) && !empty($artikelbeschreibung)) {
+if (!empty($artikelname) && !empty($preis) && !empty($artikelbeschreibung) && !empty($preis)) {
     try {
-        $statement = $db->prepare("INSERT INTO sortiment (name, beschreibung, preis, rating, bild) VALUES (:name, :beschreibung, :preis, :rating, :bild)");
-        $statement->execute(array('name' => $artikelname, 'beschreibung' => $artikelbeschreibung, 'preis' => $preis, 'rating' => $bewertung, 'bild' => $produktbild));
+        $statement = $db->prepare("INSERT INTO sortiment (name, beschreibung, genre, preis, rating, bild) VALUES (:name, :beschreibung, :genre, :preis, :rating, :bild)");
+        $statement->execute(array('name' => $artikelname, 'beschreibung' => $artikelbeschreibung, 'genre'=> $genre, 'preis' => $preis, 'rating' => $bewertung, 'bild' => $produktbild));
         $db = null;
     } catch (PDOException $e) {
         echo "Error!";
@@ -32,6 +33,8 @@ else {
 //Bild wird auf den Server geladen
 if(in_array($extension, $allowed_extensions)) {
     move_uploaded_file($_FILES['bild']['tmp_name'], '../../files/uploads/'.$_FILES['bild']['name']);
+    header('Location: ../../index.php');
+
 }
 else {
     die("Ungültige Dateiendung. Nur png, jpg, jpeg und gif-Dateien sind erlaubt");
--- functions/products/create_form.php
+++ functions/products/create_form.php
@@ -8,6 +8,7 @@ include_once ("./functions/users/admincheck.php") //Da nur Admins Produkte hinzu
         <input type="text" name="name" placeholder="Artikelname" /><br>
         <input type="text" name="preis" placeholder="Preis" /><br>
         <textarea name="beschreibung" placeholder="Artikelbeschreibung" rows="10"></textarea><br>
+        <input type="text" name="genre" placeholder="Genre" /><br>
         <input type="number" name="rating" placeholder="Bewertung" /><br>
         <input type="file" name="bild" /><br>
         <input type="submit" value="Submit"/>
