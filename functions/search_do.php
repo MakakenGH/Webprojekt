9afe5dbda52f2afaf8705234f4a85f7523035ac6
ts157@hdm-stuttgart.de <19Tbg6ejdy>
2017-11-17 14:56:35 +0100

    weweweqweqwe

--- functions/search_do.php
+++ functions/search_do.php
@@ -8,5 +8,28 @@ $db = new PDO($dsn, $dbuser, $dbpass);
 
 $sql = "SELECT * FROM sortiment WHERE name=$search";
 
+$query = $db->prepare($sql);
+$query->execute();
 
+if ($zeile = $query->fetchObject()) {
+    echo "<h1>$zeile->name</h1> <br/><br/> \n";
+    echo "$zeile->genre<br>";
+
+    $bildlg = strlen($zeile->bild);
+    if ($bildlg >= 1) {
+        echo "<img src='../../files/uploads/$zeile->bild'/><br><br> \n";
+    }
+    else {
+        echo "";
+    }
+
+    echo "$zeile->beschreibung <br/><br/> \n";
+
+
+    echo "PREIS: $zeile->preis / NUTZERWERTUNG: $zeile->rating <br><br>";
+    echo "$zeile->ean";
+}
+else
+
+    echo "Produkt nicht gefunden";
 ?>
