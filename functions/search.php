9fcea5849ca593d1f2c368a3ce4287f28b1faec1
ts157@hdm-stuttgart.de <19Tbg6ejdy>
2017-11-17 14:58:57 +0100

    weweweqweqwe

--- functions/search.php
+++ functions/search.php
@@ -1,5 +1,5 @@
 
-<form action="./functions/search_do.php"method="post" >
+<form action="search_do.php"method="post" >
     <input type="text" size="40" maxlength="250" name="search" placeholder="suche"><br>
     <input type="submit" value="Abschicken">
 
