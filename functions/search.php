0651d37fa7a8f66a5ed4d7b760fe3716e0c08d7a
ts157@hdm-stuttgart.de <19Tbg6ejdy>
2017-11-17 14:13:34 +0100

    weweweqweqwe

--- functions/search.php
+++ functions/search.php
@@ -1,8 +1,9 @@
 
 <form action="./functions/search_do.php"method="post" >
     <input type="text" size="40" maxlength="250" name="search" placeholder="suche"><br>
-</form>
+    <input type="submit" value="Abschicken">
 
+</form>
 
 
 <?php
@@ -12,4 +13,4 @@ include_once "db.php";
 $sql = "SELECT * FROM sortiment";
 
 ?>
-<
+
