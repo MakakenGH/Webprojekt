0f683ae06110f1893bf45f4419eb8ec2e7e8db69
ts157@hdm-stuttgart.de <19Tbg6ejdy>
2017-11-28 14:03:04 +0100

    weweweqweqwe

---
+++ functions/search.js
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+
+    <title>Suche mit jQuery-Autocomplete</title>
+
+<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+    <script type="text/javascript" src="js/jquery-ui-1.8.custom.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+
+    <link type="text/css" href="css/redmond/jquery-ui-1.8.custom.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    </head>
+    <body>
+    <form action="search.php">
+    <input type="text" id="search" name="search" /> <input type="submit" name="submit" value="Suchen" />
+    </form>
+    <div id="results"></div>
+    </body>
+    </html>
--- functions/search.php
+++ functions/search.php
@@ -1,26 +1,9 @@
+<script src="https://code.jquery.com/jquery-latest.js"></script>
+
 
 <form action="./functions/search_do.php"method="post" >
     <input type="text" size="40" maxlength="250" name="search" list="games" placeholder="suche">
     <input type="submit" value="Abschicken">
 
+
 </form>
-
-<datalist id="games">
-    <option value="Hitmann"/>
-    <option value="Hitmann2"/>
-    <option value="Global Strike"/>
-    <option value="Global Strike Source"/>
-    <option value=""Global Strike GO/>
-    <option value="FAFI Football 17"/>
-    <option value="TITRIS"/>
-    <option value="C&C Kapitäne"/>
-    <option value="C&C Kapitäne Stunde Eins"/>#
-    <option value="FAFI Football 18"/>
-
-
-
-
-
-
-
-</datalist>
