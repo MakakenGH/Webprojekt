4a54175ac33b71f0f5f7873426914527a3e5fafa
Tombold2000 <tr076@hdm-stuttgart.de>
2017-12-09 18:36:28 +0100

    anzahl in warenkorb jetzt möglich

--- functions/cart/cartupdate_do.php
+++ functions/cart/cartupdate_do.php
@@ -6,11 +6,12 @@ include_once("../db.php");
 $_SESSION["ean"] = $_GET["ean"];
 $ean = $_SESSION["ean"];
 $username = $_SESSION["userid"];
+$anzahl = $_GET["anzahl"];
 
 $db = new PDO($dsn, $dbuser, $dbpass);
-$sql = "INSERT INTO cart (ean, username) VALUES (:ean, :username)";
+$sql = "INSERT INTO cart (ean, anzahl, username) VALUES (:ean, :anzahl, :username)";
 $query = $db->prepare($sql);
-$query->execute(array('ean'=> $ean, "username" => $username));
+$query->execute(array('ean'=> $ean, 'anzahl' => $anzahl, "username" => $username));
 
 header("Location: ../../index.php?page=store&action=store");
 
