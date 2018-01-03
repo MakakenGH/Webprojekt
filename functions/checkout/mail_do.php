61c0497b6994bc295838e0ff0cbdb4e9aa49546f
MakakenGH <nhitzel2@gmail.com>
2018-01-03 16:41:49 +0100

    mail gesetzliche vorgaben hinzugefügt

--- functions/checkout/mail_do.php
+++ functions/checkout/mail_do.php
@@ -1,8 +1,10 @@
 <?php
+session_start();
  $new_email = $_POST["new_email"];
  $user_email = $_POST["user_email"];
 
- if(isset($user_mail)) {
+ echo $user_email;
+ if(isset($user_email)) {
      $recipient = $user_email;
  }
  else {
@@ -10,9 +12,40 @@
  }
  echo $recipient;
 
+$min = 1000;
+$max = 9999;
+$key_part1 = rand ($min ,$max);
+$key_part2 = rand ($min ,$max);
+$key_part3 = rand ($min ,$max);
+
+$key = $key_part1.'-'.$key_part2.'-'.$key_part3;
+
+$billnumber = rand ($min ,$max);
+$time = date("d-m-Y H:i:s");
+echo $time;
+
 $subject = "Dein Kauf bei Dampf!";
-$from = "From: nf025 <nf025@hdm-stuttgart.de>";
-$text = "Vielen Dank für deinen Kauf.";
+$from = "From: Dampf! <dampf@hdm-stuttgart.de>";
+$text = ("
+Vielen Dank für deinen Kauf bei Dampf!
+
+Hier ist dein Key: $key
+
+Auftragsdatum: $time
+Rechnungsnummer: $billnumber
+
+
+Diese E-Mail dient als Deine Einkaufsbestätigung.
+
+Dampf! 
+Nobelstraße 10 
+70569 Stuttgart
+Vertreten durch:
+Niklas Fath|Thomas Roglmeier|Tolga Sevim
+Umsatzsteuer-ID:123456789 
+Wirtschafts-ID: 987654321
+
+");
 
 mail($recipient, $subject, $text, $from);
 ?>
