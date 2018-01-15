b5efe3c05a7f9b801598b96a92cf86608f2403eb
Tombold2000 <tr076@hdm-stuttgart.de>
2018-01-15 11:16:55 +0100

    login form

--- functions/users/login_form.php
+++ functions/users/login_form.php
@@ -1,22 +1,28 @@
 <?php
 $_SESSION['prevprevurl'] =  $_SERVER['HTTP_REFERER'];
+
+if (!isset ($_SESSION['failed'])) {
+    $_SESSION['failed'] = 0;
+}
+
 ?>
 <div class="row">
     <div class="col-md-3 col-centered log_window">
 
-    <h4>LOGIN</h4>
-    <form action="./functions/users/login_do.php?login=1" method="post">
-    <input type="text" size="40" maxlength="250" name="username" placeholder="Login"><br>
+        <h4>LOGIN</h4>
+        <form action="./functions/users/login_do.php?login=1" method="post">
+            <input type="text" size="40" maxlength="250" name="username" placeholder="Login"><br>
 
-    <input type="password" size="40"  maxlength="250" name="password" placeholder="Dein Passwort"><br>
-<br>
-    <input type="submit" class="button_orange" value="Abschicken">
-</form>
+            <input type="password" size="40"  maxlength="250" name="password" placeholder="Dein Passwort"><br>
+            <br>
+            <input type="submit" class="button_orange" value="Abschicken">
+        </form>
         <?php
         session_start();
 
-        if ($_SESSION['failed'] = 2) {
+        if ($_SESSION['failed'] == 2) {
             echo "<br><span style='color: red;'><b>Login oder Passwort ist ungültig!</b></span>";
-        } ?>
-</div>
+        } else { echo "";}
+        ?>
+    </div>
 </div>
