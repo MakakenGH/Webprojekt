1d329d7452e44ad760067129fa140cf809ef95ad
MakakenGH <nhitzel2@gmail.com>
2018-01-17 20:53:44 +0100

    search verschoben

--- functions/search/search.php
+++ functions/search/search.php
@@ -1,7 +1,7 @@
 <script src="https://code.jquery.com/jquery-latest.js"></script>
 
 
-<form action="./functions/search_do.php"method="post" >
+<form action="./functions/search/search_do.php"method="post" >
     <input type="text" size="40" maxlength="250" name="search" list="games" placeholder="suche">
     <input type="submit" value="Abschicken">
 
--- functions/search/search_do.php
+++ functions/search/search_do.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once "db.php";
+include_once "../db.php";
 
 $search=htmlspecialchars($_POST["search"]);
 
@@ -18,7 +18,7 @@ while ($zeile = $query->fetchObject()) {
 
     $bildlg = strlen($zeile->bild);
     if ($bildlg >= 1) {
-        echo "<img src='../files/uploads/$zeile->bild'/><br><br> \n";
+        echo "<img src='../../files/uploads/$zeile->bild'/><br><br> \n";
     }
     else {
         echo "";
