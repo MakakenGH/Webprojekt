a47f6adf6f212397343e0855bb603b1395b3409d
MakakenGH <nhitzel2@gmail.com>
2018-01-17 21:45:55 +0100

    delete hinzugefügt

--- functions/backend/products/delete1.php
+++ functions/backend/products/delete1.php
@@ -1,7 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Niklas
- * Date: 30.10.2017
- * Time: 12:36
- */
+$ean = $_POST["ean"];
+echo "<span>Wirklich Löschen?</span><br><br>";
+echo "<span><a href='./functions/backend/products/delete2.php?id=$ean''>ja</span></a>          ";
+echo "<span><a href='index.php'>nein</span></a>";
+?>
