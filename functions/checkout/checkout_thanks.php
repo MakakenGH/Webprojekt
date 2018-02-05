02b82a5d2d0bc2823121c80ce85d1ee9ec4a1144
Tombold2000 <tr076@hdm-stuttgart.de>
2018-02-05 18:17:40 +0100

    tommy checkout thanks

--- functions/checkout/checkout_thanks.php
+++ functions/checkout/checkout_thanks.php
@@ -4,4 +4,4 @@ echo "<div class='col-md-4 col-centered text-center log_window' style='font-size
 echo "<img src='./files/uploads/logo_small.png' alt='DAMPF!'><br>";
 echo "<span>Vielen Dank für deinen Einkauf! </span><br><span>Du erhälst in Kürze eine Bestellbestätigung per E-Mail.</span><br><br>";
 echo "<div><a href='index.php' style='text-decoration: none;'><button class='button_orange'>Zürück zur Startseite</button></a></div>";
-echo "</div></div>";
+echo "</div></div>&nbsp";
